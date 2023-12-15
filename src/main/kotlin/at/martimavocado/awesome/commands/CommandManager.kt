7cc97cf44d3f2ba57a9b14730af8eada5bede757
martimavocado <martim.cavaco@tutanota.com>
2023-12-15 13:32:59 +0000

    remove unused commands

--- src/main/kotlin/at/martimavocado/awesome/commands/CommandManager.kt
+++ src/main/kotlin/at/martimavocado/awesome/commands/CommandManager.kt
@@ -1,70 +1,23 @@
 package at.martimavocado.awesome.commands
 
-import at.martimavocado.awesome.awesome
+import at.martimavocado.awesome.Awesome
 import at.martimavocado.awesome.commands.SimpleCommand.ProcessCommandRunnable
-import at.martimavocado.awesome.utils.ChatUtils
 import net.minecraft.command.ICommandSender
-import net.minecraft.util.BlockPos
 import net.minecraftforge.client.ClientCommandHandler
 
 class CommandManager {
 
     init {
-        registerCommand("testcommand") {
-            ChatUtils.messageToChat("Test successful.")
-        }
         registerCommand("awesome") {
-            awesome.configManager.openConfigGui()
+            Awesome.configManager.openConfigGui()
         }
         registerCommand("aw") {
-            awesome.configManager.openConfigGui()
-        }
-        registerCommand("formatmessage") { args ->
-            val colorName = args.firstOrNull()
-            if (colorName == null) {
-                ChatUtils.messageToChat("Error: Invalid usage: /formatmessage <color> <message>")
-            }
-            val colorCode = when (colorName) {
-                "red" -> "§c"
-                "blue" -> "§9"
-                "green" -> "§a"
-                "yellow" -> "§e"
-                "pink" -> "§d"
-                else -> {
-                    ChatUtils.messageToChat("Error: Invalid color '$colorName'!")
-                    return@registerCommand
-                }
-            }
-
-            val rest = args.drop(1)
-            if (rest.isEmpty()) {
-                ChatUtils.messageToChat("Error: Message can not be empty!")
-                return@registerCommand
-            }
-
-            ChatUtils.messageToChat(colorCode + rest.joinToString(" "))
+            Awesome.configManager.openConfigGui()
         }
     }
 
     private fun registerCommand(name: String, function: (Array<String>) -> Unit) {
         ClientCommandHandler.instance.registerCommand(SimpleCommand(name, createCommand(function)))
-    }
-
-    private fun registerCommand0(
-        name: String,
-        function: (Array<String>) -> Unit,
-        autoComplete: ((Array<String>) -> List<String>) = { listOf() }
-    ) {
-        val command = SimpleCommand(
-            name,
-            createCommand(function),
-            object : SimpleCommand.TabCompleteRunnable {
-                override fun tabComplete(sender: ICommandSender?, args: Array<String>?, pos: BlockPos?): List<String> {
-                    return autoComplete(args ?: emptyArray())
-                }
-            }
-        )
-        ClientCommandHandler.instance.registerCommand(command)
     }
 
     private fun createCommand(function: (Array<String>) -> Unit) = object : ProcessCommandRunnable() {

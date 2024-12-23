4bbce28b2316cadabfce12d7dbac0a217a882821
martimavocado <martim.cavaco@tutanota.com>
2024-12-23 23:18:25 +0000

    bump to 1.4

--- src/main/kotlin/at/martimavocado/awesome/events/WorldRenderEvent.kt
+++ src/main/kotlin/at/martimavocado/awesome/events/WorldRenderEvent.kt
@@ -3,5 +3,4 @@ package at.martimavocado.awesome.events
 import net.minecraftforge.fml.common.eventhandler.Cancelable
 import net.minecraftforge.fml.common.eventhandler.Event
 
-@Cancelable
 class WorldRenderEvent(val partialTicks: Float): Event()

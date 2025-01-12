75e5399f69046b1106b50a9d59acf7d27c7ea59e
martimavocado <martim.cavaco@tutanota.com>
2025-01-12 04:21:02 +0000

    aa

--- src/main/kotlin/at/martimavocado/awesome/config/categories/sheepwars/MagicWoolPerkConfig.kt
+++ src/main/kotlin/at/martimavocado/awesome/config/categories/sheepwars/MagicWoolPerkConfig.kt
@@ -12,7 +12,7 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
 class MagicWoolPerkConfig {
     @Expose
-    @ConfigOption(name = "Show GUI", desc = "Shows a GUI with the current wool's perk")
+    @ConfigOption(name = "Show GUI", desc = "Shows a GUI with the current wool's perk §c[unused]")
     @ConfigEditorBoolean
     var perkGUI = true
 
@@ -20,18 +20,25 @@ class MagicWoolPerkConfig {
     var perkPosition = GuiPosition(10, 10, HypixelGame.SHEEP_WARS)
 
     @Expose
-    @ConfigOption(name = "Ping on shoot", desc = "Pings when the wool has a good perk" +
-            "\nDisabled when not in line of sight or dead")
+    @ConfigOption(
+        name = "Ping on shoot",
+        desc =
+            "Pings when the wool has a good perk" +
+                "\nDisabled when not in line of sight or dead",
+    )
     @ConfigEditorBoolean
     var shootPing = false
 
     @Expose
-    @ConfigOption(name = "Ping Delay", desc = "Defines how often you should get pinged in ticks" + "\n§8(Magic Wool lifespan is ~40 ticks)")
+    @ConfigOption(
+        name = "Ping Delay",
+        desc = "Defines how often you should get pinged in ticks" + "\n§8(Magic Wool lifespan is 20~40 ticks)",
+    )
     @ConfigEditorSlider(minValue = 1f, maxValue = 40f, minStep = 1f)
     var pingDelay = 5
 
     @Expose
-    @ConfigOption(name = "Good Perks", desc = "Makes the highlight be the same color as the current wool §c[unused]")
+    @ConfigOption(name = "Good Perks", desc = "Makes the highlight be the same color as the current wool")
     @ConfigEditorDraggableList
     var goodPerks: MutableList<SheepWarsPowerUp> = SheepWarsPowerUp.defaultGoodPerks.toMutableList<SheepWarsPowerUp>()
 

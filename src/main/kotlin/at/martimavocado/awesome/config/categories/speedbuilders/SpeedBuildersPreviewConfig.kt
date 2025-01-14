4f68ee64b404f2f749798a841c3163f9601d22ba
martimavocado <martim.cavaco@tutanota.com>
2025-01-14 02:16:17 +0000

    having a description is also nice sometimes

--- src/main/kotlin/at/martimavocado/awesome/config/categories/speedbuilders/SpeedBuildersPreviewConfig.kt
+++ src/main/kotlin/at/martimavocado/awesome/config/categories/speedbuilders/SpeedBuildersPreviewConfig.kt
@@ -7,12 +7,12 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
 class SpeedBuildersPreviewConfig {
     @Expose
-    @ConfigOption(name = "Enabled", desc = "Highlights Magic Wool spawns")
+    @ConfigOption(name = "Enabled", desc = "Shows the solution for the current build")
     @ConfigEditorBoolean
     var enabled = true
 
     @Expose
-    @ConfigOption(name = "Block Brightness", desc = "Highlights Magic Wool spawns")
+    @ConfigOption(name = "Block Brightness", desc = "Sets the block's brightness")
     @ConfigEditorSlider(minValue = 0f, maxValue = 1f, minStep = 0.01f)
     var blockBrightness = 0.8f
 }

6b20c178645b4e28a257c526f0fc2fd631314676
martimavocado <martim.cavaco@tutanota.com>
2025-01-18 19:49:27 +0000

    make particle tracker more visual

--- src/main/kotlin/at/martimavocado/awesome/features/debug/ParticleTrackerCommand.kt
+++ src/main/kotlin/at/martimavocado/awesome/features/debug/ParticleTrackerCommand.kt
@@ -1,20 +1,26 @@
 package at.martimavocado.awesome.features.debug
 
+import at.martimavocado.awesome.events.AwesomeTickEvent
 import at.martimavocado.awesome.events.CommandRegistrationEvent
 import at.martimavocado.awesome.events.ParticleEvent
+import at.martimavocado.awesome.events.render.WorldRenderEvent
 import at.martimavocado.awesome.loadmodule.LoadModule
 import at.martimavocado.awesome.utils.ChatUtils
 import at.martimavocado.awesome.utils.SimpleTimeMark
+import at.martimavocado.awesome.utils.render.WorldRenderUtils.drawDynamicText
 import at.martimavocado.awesome.utils.system.ClipboardUtils
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 @LoadModule
 object ParticleTrackerCommand {
     private var isTracking = false
 
-    private val particleList = mutableSetOf<Pair<ParticleEvent, SimpleTimeMark>>()
+    private val particleList = mutableListOf<Pair<ParticleEvent, SimpleTimeMark>>()
     private var trackingStartTime = SimpleTimeMark.farPast()
+
+    private var particlesToRender: List<Pair<ParticleEvent, SimpleTimeMark>> = emptyList()
 
     @SubscribeEvent
     fun onParticle(event: ParticleEvent) {
@@ -35,6 +41,7 @@ object ParticleTrackerCommand {
         if (!isTracking) {
             ChatUtils.chat("Started tracking particles!")
             particleList.clear()
+            particlesToRender = emptyList()
             trackingStartTime = SimpleTimeMark.now()
             isTracking = true
 
@@ -51,6 +58,33 @@ object ParticleTrackerCommand {
 
         isTracking = false
         particleList.clear()
+        particlesToRender = emptyList()
         trackingStartTime = SimpleTimeMark.farPast()
+    }
+
+    @SubscribeEvent
+    fun onTick(event: AwesomeTickEvent) {
+        if (!isTracking) return
+
+        particlesToRender = particleList.toList()
+    }
+
+    @SubscribeEvent
+    fun onWorldRender(event: WorldRenderEvent) {
+        if (!isTracking) return
+
+        val particles =
+            particlesToRender
+                .associate { it.first to it.second.passedSince() }
+                .toMap()
+                .filter { it.value < 3.seconds }
+
+        particles.forEach {
+            event.drawDynamicText(
+                it.key.location,
+                it.key.type.toString(),
+                0.5,
+            )
+        }
     }
 }
